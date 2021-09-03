ddf9e05c630fbe068832374754e86e9d085bfa88
ish <ismael.veyna@gmail.com>
2021-09-02 21:07:07 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace EmguCVTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(60, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(742, 475);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(172, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "START";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(253, 12);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(75, 23);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "PAUSE";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(334, 12);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "STOP";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 608);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Windows.Forms;
+
+namespace EmguCVTutorial
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture device;
+        public Form1()
+        {
+            InitializeComponent();
+            device = new VideoCapture(0);
+            
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (device == null)
+                device = new VideoCapture(0); 
+
+            device.ImageGrabbed += Capture_ImageGrab;
+            device.Start();
+
+
+
+        }
+
+        private void Capture_ImageGrab(object sender, EventArgs e)
+        {
+            if (device != null)
+            {
+                try
+                {
+                    Mat m = new Mat();
+                    device.Retrieve(m);
+                    pictureBox1.Image = m.ToImage<Bgr, byte>().ToBitmap();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+            else
+            {
+                Mat m = new Mat();
+                pictureBox1.Image = m.ToImage<Bgr, byte>().ToBitmap();
+            }
+            
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if (device != null)
+                device.Pause();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (device != null)
+                device = null;
+        }
+    }
+}
